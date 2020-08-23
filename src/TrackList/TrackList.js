feb633d8adb8b04a33573125e3ac99ce4102b09b
NBESS <nicholasdbess@gmail.com>
2020-08-23 13:44:10 -0400

    Add boilerplate for components js and css

---
+++ src/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class TrackList extends Component {
+    render() {
+        return (
+            <div class="TrackList">
+                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+            </div>
+        )
+    }
+}
+
+
